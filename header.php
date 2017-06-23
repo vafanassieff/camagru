e288197303cacc239a50782d66327b2db81927cf
Victor Afanassieff <vafanass@student.42.fr>
2017-06-23 16:21:48 +0200

    Folder and files

---
+++ header.php
@@ -0,0 +1,11 @@
+<head>
+  <meta charset="utf-8">
+  <link rel="icon" href="install_files/oscar.ico" />
+  <link rel="stylesheet" type="text/css" href="css/all.css">
+  <title>Camagru</title>
+</head>
+<body>
+<div id="center">
+<header>
+<a href="index.php" class="menu">Boutique</a>
+</header> 
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+include('header.php');
+include('footer.php');
+?>
+
+<!DOCTYPE html>
+<html>
+
+toto
+</body>
+
+</html>
