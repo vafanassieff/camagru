9701a59221bebadffafe29975e90695b9b52e69d
Victor Afanassieff <vafanass@student.42.fr>
2017-07-07 16:40:03 +0200

    css fix

--- function/comment.php
+++ function/comment.php
@@ -2,9 +2,9 @@
 
 function comment_form(){
 
-	echo '<div class="form-style-10">
-			<textarea name="comment" form="commentform" rows="4" cols="50">Enter text here...</textarea>
-			<form action="gallery.php" method="POST" id="commentform">
+	echo '<div class="form-style-10">';
+	//echo '<textarea name="comment" form="commentform" rows="4" cols="50">Enter text here...</textarea>';
+	echo '		<form action="gallery.php" method="POST" id="commentform">
     			<div class="button-section">
      				<center><input type="submit" name="submit" value="OK"/></center>
 				<div class="button-section">
